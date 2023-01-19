8a5e29cfcd5938ccc2952afcf532962dafeccd58
Gokhan Demirer <gokhan.demirer@siontek.io>
2023-01-19 14:48:23 +0300

    Create transaction model and related migration

---
+++ database/migrations/2023_01_19_114703_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('order_id');
+            $table->string('ref_code');
+            $table->float('price');
+            $table->float('point');
+            $table->string('hash');
+            $table->string('callback_success_url');
+            $table->string('callback_fail_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
