cc8ca005bb598057b9ba4e0a4f5c7fd6803b4e48
Gokhan Demirer <gokhan.demirer@siontek.io>
2023-01-19 14:59:38 +0300

    Handle openGate request in GateController, save the transaction to database through repository

--- app/Http/Controllers/GateController.php
+++ app/Http/Controllers/GateController.php
@@ -2,20 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes\Constants\RequestParam;
+use App\Classes\Utils\HashGenerator;
+use App\Classes\Utils\ResponseManager;
+use App\Exceptions\TransactionNotCreatedException;
 use App\Http\Requests\Gate\OpenGateRequest;
-use Illuminate\Http\Request;
+use App\Interfaces\TransactionRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
 
 class GateController extends Controller
 {
-    public function __construct()
+    /**
+     * @param TransactionRepositoryInterface $transactionRepository
+     */
+    public function __construct(
+        public TransactionRepositoryInterface $transactionRepository
+    )
     {
         $this->middleware([
             'verified.hash'
         ]);
     }
 
-    public function openGate(OpenGateRequest $request)
+    /**
+     * @param OpenGateRequest $request
+     * @return JsonResponse
+     * @throws TransactionNotCreatedException
+     */
+    public function openGate(OpenGateRequest $request): JsonResponse
     {
+        $requestParam = RequestParam::getInstance();
+        $callbackFailUrl = $requestParam->getCallbackFailUrl();
+        $callbackSuccessUrl = $requestParam->getCallBackSuccessUrl();
+        $callbackHash = HashGenerator::getCallbackHash();
 
+        try {
+            $this->transactionRepository->createTransaction(
+                $request->input('user_id'),
+                $request->input('order_id'),
+                $request->input('ref_code'),
+                $requestParam->getPrice(),
+                $request->input('point'),
+                $request->input('hash'),
+                $callbackSuccessUrl,
+                $callbackFailUrl
+            );
+
+            // Success
+            $this->sendCallbackRequest($callbackHash, true);
+            return ResponseManager::getGenericResponse('Success');
+        } catch (TransactionNotCreatedException $exception) {
+            // Fail
+            $this->sendCallbackRequest($callbackHash, false);
+            throw $exception;
+        }
+    }
+
+    /**
+     * @param string $hash
+     * @param bool $isSuccess
+     * @return void
+     */
+    private function sendCallbackRequest(string $hash, bool $isSuccess): void
+    {
+        $requestParam = RequestParam::getInstance();
+        Http::contentType('application/json')->post(
+            $isSuccess ? $requestParam->getCallBackSuccessUrl() : $requestParam->getCallbackFailUrl(),
+            [
+                'hash' => $hash
+            ]
+        );
     }
 }
